80164fe8d74836b4759e15b417f2e3f5a5e12fbf
jamesgawn <james@gawn.uk>
2020-10-08 22:53:20 +0100

    Added storage of new films that become available for booking as they are discovered

--- src/lambda.spec.ts
+++ src/lambda.spec.ts
@@ -1,8 +1,50 @@
 import {handler} from "./lambda";
+import {SimpleShowing} from "./service/domain/SimpleShowing";
+import {Film} from "./lib/OdeonApi/odeonTypes";
+import {add} from "date-fns";
+
+const mockGetNextShowingByFilmForCinema = jest.fn();
+jest.mock("./service/CinemaInfoService", () => ({
+  CinemaInfoService: jest.fn().mockImplementation(() => ({
+    getNextShowingByFilmForCinema: mockGetNextShowingByFilmForCinema
+  }))
+}));
+const mockGetRecordById = jest.fn();
+const mockPutRecord = jest.fn();
+jest.mock("./lib/DynamoDBHelper", () => ({
+  DynamoDBHelper: jest.fn().mockImplementation(() => ({
+    getRecordById: mockGetRecordById,
+    putRecord: mockPutRecord
+  }))
+}));
 
 describe("lambda.ts", () => {
+  const film1 = {
+    "id": "filmid1",
+    "title": {
+      "text": "Film 1",
+    }
+  } as Film;
+  const film2 = {
+    "id": "filmid2",
+    "title": {
+      "text": "Film 2",
+    }
+  } as Film;
+  const showing1 = new SimpleShowing("film1", film1, add(new Date(), {days: 1}));
+  const showing2 = new SimpleShowing("film2", film2, add(new Date(), {days: 2}));
   test("handler", async () => {
+    mockGetNextShowingByFilmForCinema.mockResolvedValue(new Map<string, SimpleShowing>([
+      ["film1", showing1],
+      ["film2", showing2]
+    ]));
+    mockGetRecordById.mockResolvedValueOnce(film1);
+    mockGetRecordById.mockResolvedValueOnce(undefined);
     const result = await handler();
-    expect(result).toBe("Hello World");
+    expect(mockGetRecordById).toHaveBeenNthCalledWith(1, "film1");
+    expect(mockGetRecordById).toHaveBeenNthCalledWith(2, "film2");
+    expect(mockPutRecord).toHaveBeenCalledTimes(1);
+    expect(mockPutRecord).toBeCalledWith(showing2.toRecord());
+    console.log(result);
   });
 });
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,8 +1,18 @@
-// import {CinemaInfoService} from "./service/CinemaInfoService";
+import {CinemaInfoService} from "./service/CinemaInfoService";
+import {DynamoDBHelper} from "./lib/DynamoDBHelper";
+import {FilmRecord} from "./domain/FilmRecord";
 
 export const handler = async ()
     : Promise<string> => {
-  // const cis = new CinemaInfoService();
-  // const upcomingFilms = await cis.getNextShowingByFilmForCinema(150, new Date(), 2);
+  const cis = new CinemaInfoService();
+  const dbh = new DynamoDBHelper("bfi-film-showings");
+  const upcomingFilms = await cis.getNextShowingByFilmForCinema(150, new Date(), 2);
+  for (const [filmId, showing] of upcomingFilms) {
+    const persistedFilm = await dbh.getRecordById<FilmRecord>(filmId);
+    // If not previously seen, then do things
+    if (!persistedFilm) {
+      await dbh.putRecord<FilmRecord>(showing.toRecord());
+    }
+  }
   return "Hello World";
 };
