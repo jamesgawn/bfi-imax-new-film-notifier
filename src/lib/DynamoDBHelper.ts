cfe8f40dbe2fcf0fbe08b4562942e1578fccbfcb
jamesgawn <james@gawn.uk>
2020-10-09 10:36:47 +0100

    Added new base class to add logging capability to other classes. Completed example class with logging enhancements.

--- src/lib/DynamoDBHelper.ts
+++ src/lib/DynamoDBHelper.ts
@@ -1,11 +1,13 @@
 import {DynamoDB} from "aws-sdk";
 import {FriendlyError} from "./FriendlyError";
 import {IRecord} from "../domain/IRecord";
+import {Base} from "./Base";
 
-export class DynamoDBHelper {
+export class DynamoDBHelper extends Base {
   tableName: string;
   client: DynamoDB.DocumentClient;
   constructor(tableName: string) {
+    super();
     this.tableName = tableName;
     this.client = new DynamoDB.DocumentClient({
       convertEmptyValues: true
@@ -20,9 +22,12 @@ export class DynamoDBHelper {
           "id": id,
         }
       };
+      this.log.info(`Retrieving record ${id}`);
       const result = await this.client.get(params).promise();
+      this.log.info(`Retrieved record ${id}`);
       return result.Item as T;
     } catch (err) {
+      this.log.error(`Failed to retrieve record ${id}`, err);
       throw new FriendlyError(`Unable to retrieve record ${id}`, err);
     }
   }
@@ -33,8 +38,12 @@ export class DynamoDBHelper {
       TableName: this.tableName,
     };
     try {
-      return await this.client.put(params).promise();
+      this.log.info(`Putting record ${record.id}`);
+      const result = await this.client.put(params).promise();
+      this.log.info(`Put record ${record.id}`);
+      return result;
     } catch (err) {
+      this.log.error(`Failed to put record ${record.id}`, err);
       throw new FriendlyError(`Unable to store details of record ${record.id}`, err);
     }
   }
